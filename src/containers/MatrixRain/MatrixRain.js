6623b3f38a9a984ee28fe3f068301c3004b0f424
QamarFarooq <ch.farooq.qamar@hotmail.com>
2022-06-01 22:54:18 -0400

    Revert "added matrix rain glow effect successfully"
    reverting to old file before i merged matrix_ran_glow_effect
    
    This reverts commit 6ac265a587c423875278aecbafb19f67e288d9a5.

--- src/containers/MatrixRain/MatrixRain.js
+++ src/containers/MatrixRain/MatrixRain.js
@@ -4,114 +4,55 @@ import RainStream from './RainStream.js';
 import {Context} from '../../store/Store.js';
 
 
+
 const MatrixRain = ( props ) => {
     const canvasRef = useRef(null);
-
+  
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-
-        const fontSize = 14
-
-        // any number less than 0.5 will cause columns to start to overlap
-        const xAxisDistanceBetweenCols = 0.8
-
-        const yAxisDistanceBetweenRows = 1.1
-
-        const matrixCharTracker = {
-            prevChar: "",
-            currChar: "",
-            currXCoordinate: 0,
-            currYCoordinate: 0, // the * 2 is there for testing purposes, remove it later
-            fontSize: fontSize,
-            alphabet: `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`,
-
-            generateChar: function() {
-
-                // first row edge case
-                if (this.currYCoordinate === 0) {
-                    
-                    context.fillStyle = 'white'; // white
-                    context.font = '900 ' + fontSize + 'px matrixFont';
-    
-                    // generate random char
-                    this.currChar = this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length));
-    
-                    // draw newly generated char
-                    // context.fillText(text,x,y,maxWidth);
-                    context.fillText(this.currChar, this.currXCoordinate*this.fontSize, this.currYCoordinate*this.fontSize, 10);
-
-                    // save current char as previous char
-                    this.prevChar = this.currChar;
-                }
-                // every other row case
-                else {
-                    // remove top char(prev char)
-                    context.fillStyle = 'black'; // black
-                    // context.fillRect(x,y,width,height);
-                    context.fillRect(this.currXCoordinate*this.fontSize, (this.currYCoordinate-1.90)*this.fontSize, 11.5, this.fontSize+0.2);
-
-                    // rewrite top char(prev char) as green char
-                    context.fillStyle = '#0F0'; // green
-                    context.font = '900 ' + fontSize + 'px matrixFont';
-                    // context.fillText(text,x,y,maxWidth); this was 10 before
-                    // the higher the mult for this.currYCoordinate, the more it will move down
-                    context.fillText(this.prevChar, this.currXCoordinate*this.fontSize, (this.currYCoordinate-yAxisDistanceBetweenRows)*this.fontSize, 10);
-    
-                    // generate new random char
-                    context.fillStyle = 'white'; // white
-                    context.font = '900 ' + fontSize + 'px matrixFont';
-                    this.currChar = this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length));
-
-                    // draw newly generated char
-                    // context.fillText(text,x,y,maxWidth);
-                    context.fillText(this.currChar, this.currXCoordinate*this.fontSize, this.currYCoordinate*this.fontSize, 10);
-
-                    // save current char as previous char
-                    this.prevChar =  this.currChar;
-                }
-            }
-        };
         
-        const matrixRain = [];
+        const alphabet = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
         
-        // i < 1 is just there so you can have two columns 
-        // for testing, change it back to canvas.length later
-        for( let x = 0; x < canvas.width; x += xAxisDistanceBetweenCols ) {
-
-            const matrixRainCol = Object.create(matrixCharTracker);
-            matrixRainCol.currXCoordinate = x;
-
-            matrixRain[x] = matrixRainCol;
+        const fontSize = 15;
+        const columns = canvas.width/fontSize;
+        
+        const rainDrops = [];
+        
+        for( let x = 0; x < columns; x += 0.5 ) {
+            //the index val represents the x coordinate, and the val 
+            //at the index represent the y coordinate
+            rainDrops[x] = 1;
         }
+
+        console.log(rainDrops);
         
         const draw = () => {
-            // draw really light black background over entire canvas
             context.fillStyle = 'rgba(0, 0, 0, 0.05)';
             context.fillRect(0, 0, canvas.width, canvas.height);
             
-            // i < 1 is just there so you can have two columns 
-            // for testing, change it back to canvas.length later
-            for(let i = 0; i < canvas.width; i += xAxisDistanceBetweenCols)
+            context.fillStyle = '#0F0';
+            context.font = 'bolder ' + fontSize + 'px matrixFont';
+        
+            for(let i = 0; i < rainDrops.length; i += 0.5)
             {
-                // generate char
-                matrixRain[i].generateChar();
-
-                // if column reaches end of screen, randomly reset some column to re-start from top
-                if(matrixRain[i].currYCoordinate*fontSize > canvas.height && Math.random() > 0.975){
-                    matrixRain[i].currYCoordinate = 0;
+                const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+                context.fillText(text, i*fontSize, rainDrops[i]*14);
+                
+                //reset the y coordinate randomly if it is found that rain
+                //drop has gone the beyond height of window
+                if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+                    rainDrops[i] = 0;
                 }
-            
-                // increment y coordinates to make rain go down
-                matrixRain[i].currYCoordinate = matrixRain[i].currYCoordinate + yAxisDistanceBetweenRows;
+                //increment the element at rainDrop[i] by 1, which is the y cord
+                rainDrops[i]++;
             }
         };
-
         //value of 50 looks good
-        setInterval(draw, 90);
+        setInterval(draw, 100);
     }, [])
 
     return (
